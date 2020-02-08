484bd0ea8c52c09e20ec48e724c76efbd76a3232
Jérémy Gotteland <jeremy.gotteland@gmail.com>
2020-02-08 15:49:48 +0100

    Scope: Transient is not relevant here

--- src/api.service.ts
+++ src/api.service.ts
@@ -25,7 +25,7 @@ export function sendTokenRequestByCredentials(
   });
 }
 
-@Injectable({ scope: Scope.TRANSIENT })
+@Injectable()
 export default class APIService {
   private axios: AxiosInstance;
 
