7531409ffe79a92b2e1377480f3a8d2cd6bc4656
Jérémy Gotteland <jeremy.gotteland@gmail.com>
2020-02-14 08:29:03 +0100

    Specify date format

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,6 +4,7 @@ export interface VisitData {
   id: string;
   care_recipient_id: string;
   caregiverIds: string[];
+  // in ISO format 2019-10-15T00:05:32.000Z
   start_at: string;
   end_at: string;
 }
